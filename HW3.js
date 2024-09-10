70bc9b9a509c142eb3ea46b62fc76fec885a5ac8
biedronna <annaromanowicz7@gmail.com>
2024-09-10 10:58:08 +0200

    Add new files

---
+++ HW3.js
@@ -0,0 +1,15 @@
+//zadanie 1 :
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+
+//zadanie 2 :
+
+const numbers = [4,9,5,7,3,8,9];
+
+for( let i=0; i< numbers.length; i+=1) {
+
+    if (numbers[i]> 5) {
+        console.log(numbers[i]); 
+    }
+ 
+}
+
